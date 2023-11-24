59aab9ea3f30925e9f04ef3549d8fd2a08703c17
SYEONIH <113624562+SYEONIH@users.noreply.github.com>
2023-11-24 16:08:57 +0900

    Add files via upload

---
+++ routes/create-account.tsx
@@ -0,0 +1,75 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import { FirebaseError } from "firebase/app";
+import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-components";
+import GithubButton from "../components/github-btn";
+
+
+// HOOKS
+export default function CreateAccount() {
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "name") {
+            setName(value);
+        } else if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault(); // 새로고침 금지
+        setError("");
+        if (isLoading || name === "" || email === "" || password === "") return;
+        try {
+            setLoading(true);
+            const credentials = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(credentials.user);
+            await updateProfile(credentials.user, {
+                displayName: name,
+            });
+
+            navigate("/");
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+                setError(e.message);
+            }
+        }
+        finally {
+            setLoading(false);
+        }
+
+
+    }
+
+    return (
+        <Wrapper>
+            <Title>Join 😶‍🌫️</Title>
+            <Form onSubmit={onSubmit}>
+                <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" />
+                <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+                <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+                <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
+            </Form>
+            {error !== "" ? <Error>{error}</Error> : null}
+            <Switcher>
+                Already have an account?
+                <Link to="/login">Log in &rarr;</Link>
+            </Switcher>
+            <GithubButton />
+        </Wrapper>
+    );
+}
